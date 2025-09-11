2bb22151716c4bf3c90afd36dac7f90663fbf5a6
arbajkhan23 <126312082+arbajkhan23@users.noreply.github.com>
2025-09-11 06:43:06 +0000

    variable

--- javascript.js
+++ javascript.js
@@ -1,2 +1,52 @@
+let age = "18";
+const data ="2025";
+var name = "arbaj khan"
+accounetCity = "jaipur"
+console.table([age , data ,name , accounetCity])
 
-console.log("arbaj khan")
+/* Prefer not to use var 
+beacuse of issue in block scope and fuctional scope */
+
+
+// let a = "250";
+// let num = Number(a);
+// console.log(num);        
+// console.log(typeof num);
+
+// Q2: Convert number 300 into a string
+// let b = 300;
+// let num1 = string(b);
+// console.log(typeof num1);
+
+
+
+// *************** Operations ************************
+
+let value = 3
+let  negValue = -value
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " arbaj"
+
+let str3 = str1 + str2 
+console.log(str3);
+
+//  console.log("1" + 2);
+//  console.log(1 + "2" );
+//  console.log("1" + 2 + 2);
+//  console.log(1 + 2 + "2");
+
+// console.log (+true);
+// console.log (+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
