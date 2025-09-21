147c9694ea2aa23c69e430851869f7988b7f0ef7
arbajkhan23 <126312082+arbajkhan23@users.noreply.github.com>
2025-09-21 17:32:13 +0000

    datatype-summary

---
+++ datatyps-summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 types : String, Boolean , Number, null, BigInt , Symbol,  undefined,
+
+const score = 100
+ const scoreValue = 100.3
+ 
+ 
+ const isLoggedIn = false
+ const outsideTemp = null;
+ let userEmail;
+ console.log(typeof outsideTemp);
+ const id = Symbol ('123')
+ const anotherId = Symbol('123');
+ console.log(id === anotherId);
+ console.log(typeof anotherId);
+ const bigNumber = 2482764847778457548746847n;
+console.log(typeof bigNumber);
+
+
+ // Referenc (Noe Primitive)
+// Array
+// Object
+// Function
+
+
+// const heros = ["shaktiman", "nagggraj", "doag"];   
+
+// let 
+//    myObj {
+//     name : "arbaj khan" ;
+//     age : 21;
+//    }
+
+
+const myFunction = function() {
+    console.log("Hello world");
+}
+
+console.log(typeof myFunction);
+
