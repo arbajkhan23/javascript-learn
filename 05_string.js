b2e9c43c8442fad109bc29b1718a500570f6e299
arbajkhan23 <126312082+arbajkhan23@users.noreply.github.com>
2025-09-25 17:41:52 +0000

    string reading

---
+++ 05_string.js
@@ -0,0 +1,17 @@
+const name = "arbaj" ;
+const repoCout = 14 ;
+
+// console.log(name + repoCout + "value")
+console.log(`Hello my name is ${name} and my repa count is ${repoCout}`);
+
+const gameName = new String ('freefire-arbaj')
+console.log(gameName[0]);
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.toLocaleUpperCase());
+console.log(gameName.slice());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('e'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
